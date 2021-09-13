cf7d095f2ebe38790a6a3638513c4d3316c14cc3
Jenny Kim <kimj1905@gmail.com>
2021-09-13 14:32:01 -0400

    commit tree c82472dfa2e52a6ad5b88370532387aae1237f1b

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Your code here
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`
+}
+
+const mondayWork = function (activity = "go to the office") {
+    return `This Monday, I will ${activity}.`
+}
+
+function wrapAdjective(str = "*") {
+    return function (adj = "special") {
+        return `You are ${str}${adj}${str}!`
+    }
+}
+
+// console.log(wrapAdjective()())
+
+const Calculator = {
+    add: (x, y) => x + y,
+    subtract: (x, y) => x - y,
+    multiply: (x, y) => x * y,
+    divide: (x, y) => x / y
+}
+
+function actionApplyer(start, arr) {
+    if (arr.length === 0) {
+        return start
+    } else {
+        for (const func of arr) {
+            start = func(start)
+        }
+        return start
+    }
+}
